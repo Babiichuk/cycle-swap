4e4d8c63eecd338ea011efd4cd3fa14ba4fe499e
Babiichuk <heorhii.babiichuk@gmail.com>
2022-02-05 19:00:02 +0200

    CycleSwap done

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -1,11 +1,34 @@
 package com.epam.rd.autotasks;
 
 class CycleSwap {
+
     static void cycleSwap(int[] array) {
+        if (array.length != 0) {
+            int temp = array[0];
+            array[0] = array[array.length - 1];
+            for (int i = 1; i < array.length - 1; i++) {
+                array[array.length - i] = array[array.length - i - 1];
+            }
+            array[1] = temp;
+        }
 
     }
 
+
     static void cycleSwap(int[] array, int shift) {
+        if (array.length != 0) {
+            for (int n = 0; n < shift; n++) {
+                int temp = array[0];
+                array[0] = array[array.length - 1];
+
+
+                for (int i = 1; i < array.length - 1; i++) {
+                    array[array.length - i] = array[array.length - i - 1];
+                }
+                array[1] = temp;
+            }
+        }
 
     }
 }
+
